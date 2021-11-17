9b50791222741620646d2f1c8631838b8dad121b
youssif hany <youssifhany75@gmail.com>
2021-11-17 17:56:41 +0200

    add rounds

--- script.js
+++ script.js
@@ -2,5 +2,26 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
 
+const compareGuesses = (humanGuess, computerGuess, secretNumber) => {
+  const humanMargin = getAbsoluteDistance(humanGuess, secretNumber);
+  const computerMargin = getAbsoluteDistance(computerGuess, secretNumber);
+  if (humanMargin === computerMargin) return true;
+  return humanMargin < computerMargin;
+};
+
+const updateScore = (winner) => {
+  switch (winner) {
+    case "human":
+      return humanScore++;
+    default:
+      return computerScore++;
+  }
+};
+
+const advanceRound = () => currentRoundNumber++;
+
+const getAbsoluteDistance = (src, dist) => Math.abs(dist - src);
